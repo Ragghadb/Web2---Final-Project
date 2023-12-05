51eb079779c1c4c7404ccf7dd1d891d9a2c12879
ReemKhleiwi <ReemKleiwi@gmail.com>
2023-12-06 02:13:19 +0300

    1

--- FinalPtoject/Controllers/ordersController.cs
+++ FinalPtoject/Controllers/ordersController.cs
@@ -42,12 +42,22 @@ namespace FinalPtoject.Controllers
 
         public async Task<IActionResult> report()
         {
-            var orItems = await _context.itemlsst.FromSqlRaw
+            var orItems = await _context.items.FromSqlRaw
                 ("select usersall.id as Id, usersall.name as customername, " +
-                "sum(quantity * price) as total from itemsall, orders, usersall where  itemid = itemsall.Id, and custid = usersall.Id group by usersall.id, usersall.name ").ToListAsync();
+                "sum(quantity * price) as total from itemsall," +
+                " orders, usersall where  itemid = itemsall.Id, and custid =" +
+                " usersall.Id group by usersall.id, usersall.name ").ToListAsync();
             return View(orItems);
 
         }
+
+public async Task<IActionResult> myprunchase()
+        {
+            int userid = Convert.ToInt32(HttpContext.Session.GetString("userid"));
+            var orItems = await _context.orders.FromSqlRaw("select *  from orders where  userid = '" + userid + "'  ").ToListAsync();
+            return View(orItems);
+        }
+
 
         // GET: orders/Details/5
         public async Task<IActionResult> Details(int? id)
